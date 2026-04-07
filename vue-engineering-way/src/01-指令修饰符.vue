9cea913027546bd473ab284a0898ce5205802504
xiaoli79 <3099592185@qq.com>
2026-04-07 20:35:42 +0800

    提交

---
+++ vue-engineering-way/src/01-指令修饰符.vue
@@ -0,0 +1,71 @@
+
+<script setup>
+
+
+  // // 处理按键函数
+  // const onKeyDown = () =>{
+  //   console.log('onKeyDown')
+  // }
+
+  const onPClick = () => {
+    console.log("onPClick")
+  }
+
+
+  const onDivClick = () => {
+    console.log("onDivClick")
+  }
+
+
+</script>
+
+<template>
+  <div @click="onDivClick">
+    <!-- 只有回车才会触发按键修饰符 -->
+    <!-- <input 
+      type="text" 
+      @keydown.enter="onKeyDown"> -->
+
+    <!--.prevent: 阻止默认行为  -->
+    <a 
+      href="https://baidu.com"
+      @click.prevent>
+      百度一下
+    </a>
+    <!-- 这里面的click.stop是禁止向上传递，也就是阻止冒泡 -->
+    <p @click.stop="onPClick"></p>
+
+
+    <!-- 修饰符的链式调用: 表明两个同时阻止 -->
+    <a 
+      href="https://baidu.com"
+      @click.prevent.stop>
+      百度一下
+    </a>    
+  </div>
+
+
+
+</template>
+
+
+<style scoped>
+    div {
+        width: 400px;
+        height: 200px;
+        background: plum;
+    }
+    div a {
+        display: block;
+        width: 100px;
+        text-decoration: none;
+        background: tomato;
+        text-align: center;
+        color: #fff;
+    }
+    div p {
+        width: 200px;
+        height: 100px;
+        background: rebeccapurple;
+    }
+</style>
---
+++ vue-engineering-way/src/03-v-model用在表单元素.vue
@@ -0,0 +1,62 @@
+<script setup>
+  import { ref } from 'vue';
+
+  //收集自我介绍的响应式数据
+  const intro = ref('')
+
+  //收集城市的响应式数据
+  const city = ref('')
+
+  const blood = ref('')
+
+  const isAgree = ref('')
+
+  const hobby = ref(["YLJ","HS"])
+
+</script>
+
+<template>
+  <div> 
+    <!-- 文本域 -->
+    <textarea cols="20" rows="20" placeholder="请输入自我介绍" v-model="intro"></textarea>
+    <br/>
+    <br/>
+  </div>
+
+  <!-- 下拉菜单 -->
+  <select v-model="city">
+    <option value="beijing">北京</option>
+    <option value="shanghai">上海</option>
+    <option value="shengzhen">深圳</option>
+    <option value="hangzhou">杭州</option>
+  </select>
+
+  <br/>
+  <br/>
+
+
+  <!-- 单选框 -->
+  <input type="radio" value="a" v-model="blood"> A
+  <input type="radio" value="b" v-model="blood"> B
+  <input type="radio" value="ab" v-model="blood"> AB
+  <input type="radio" value="o" v-model="blood"> O
+
+  <br>
+  <br>
+
+  <!-- 复选框 -->
+  <input type="checkbox" v-model="isAgree">是否同意协议
+
+  <br>
+  <br>
+
+  <input type="checkbox" value="NL" v-model="hobby">奶龙
+  <input type="checkbox" value="TS" v-model="hobby">豚鼠
+  <input type="checkbox" value="YLJ" v-model="hobby">闫路甲
+  <input type="checkbox" value="HS" v-model="hobby">韩硕
+
+</template>
+
+<style scoped>
+
+</style>
