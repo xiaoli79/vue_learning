b28f68b6f72b2dfca094e1be865e8d58143ee381
xiaoli79 <3099592185@qq.com>
2026-04-21 23:00:48 +0800

    vue学习

---
+++ vue-engineering-way/src/10-计算属性的完整写法.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { computed } from 'vue';
+
+
+
+
+
+// //简写 计算属性只读不可改
+// const uname = computed(()=>{
+//     return '姬霓太美'
+//  })
+   const uname = computed({
+    get(){
+      return '姬霓太美'
+    },
+    
+    //要给计算属性赋予的新值
+    //只要给计算赋值，都会触发set函数去执行
+    set(value){
+      console.log(value)
+    }
+
+   })
+</script>
+
+
+
+<template>
+  <div>
+    <input 
+      type="text"
+      v-model="uname">
+
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
