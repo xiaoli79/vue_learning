f76475b52fd89f5bf8138042c844a52935478224
xiaoli79 <3099592185@qq.com>
2026-05-06 21:59:56 +0800

    vue组件通信的学习

---
+++ vue-engineering-way/05-props的使用/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+    //导入组件
+    import BitDialog from './components/bit-dialog.vue';
+    
+</script>
+
+
+
+<template>
+    <!-- <bit-dialog :opacity="0.5"/> -->
+    <!-- <bit-dialog :opacity="100"/> -->
+
+</template>
+
+
+<style scoped>
+
+
+</style>
---
+++ vue-engineering-way/src/components/TodoFooter.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import { computed } from 'vue';
+
+
+  const emit = defineEmits()
+
+
+  const props = defineProps({
+    todoList:{
+      type:Array,
+      default: () => []
+    } 
+  })
+
+  const unFinishedLength = computed(()=>{
+    return props.todoList.filter((item) => !item.finished).length
+  })
+
+
+
+</script>
+
+<template>
+  <footer class="footer">
+    <span class="todo-count"><strong>{{ unFinishedLength }}</strong> item left</span>
+    <ul class="filters">
+      <li><a href="#/" class="selected">All</a></li>
+      <li><a href="#/active">Active</a></li>
+      <li><a href="#/completed">Completed</a></li>
+    </ul>
+    <button class="clear-completed" @click="emit('clear-todo')">Clear completed</button>
+  </footer>
+</template>
