20e66c1ed5879203fa908f2f5d75607a6ddc8b57
xiaoli79 <3099592185@qq.com>
2026-05-08 12:43:53 +0800

    v-model原理

---
+++ vue-engineering-way/01-v-model原理/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { ref } from 'vue';
+  const msg = ref('')
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <!-- <input 
+    type="text"
+    v-model="msg">
+   -->
+
+  <!-- :value实现数据的变化 -->
+  <input 
+    type="text"
+    :value="msg"
+    @input="msg=$event.target.value"/>
+
+</template>
+
+<styles scoped>
+
+
+ </styles>
