1d2208395d45f01aa3493aabdd75648cc8633150
xiaoli79 <3099592185@qq.com>
2026-05-13 12:31:03 +0800

    提交ref

---
+++ vue-engineering-way/06-ref实现图表/App.vue
@@ -0,0 +1,18 @@
+
+
+<script setup>
+  import BitChart from './component/bit-chart.vue';
+</script>
+
+<template>
+  <div>
+    <bit-chart/>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+
+</style>
