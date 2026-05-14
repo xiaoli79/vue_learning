a076f9a0ecf9c5e0243ed713d5ee5165219f3c2e
xiaoli79 <3099592185@qq.com>
2026-05-14 15:00:05 +0800

    提交

---
+++ 06-v-model进阶+ref+nextTick/07-ref作用于组件实例/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import { onMounted, ref } from 'vue';
+    import MyForm from './components/MyForm.vue';
+
+    const formRef = ref(null)
+    onMounted(()=>{
+        console.log(formRef.value.validate())
+    })
+
+    const onLogin = () =>{
+        if(formRef.value.validate()){
+            console.log('ok!')
+        }else{
+            console.log('不 ok!')
+        }
+    }
+</script>
+
+<template>
+
+    <div class="login-box">
+        <!-- ref获取组件实例 -->
+        <MyForm ref="formRef"/>
+        <button @click="onLogin">登录</button>
+    </div>
+    
+</template>
+
+<style scoped>
+
+
+</style>
