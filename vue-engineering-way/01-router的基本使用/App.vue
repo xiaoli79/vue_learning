3ace29c1ba71b4ba7479af0132daf816ece36647
xiaoli79 <3099592185@qq.com>
2026-05-28 16:36:44 +0800

    vue学习

---
+++ vue-engineering-way/01-router的基本使用/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+
+</script>
+
+
+
+<template>
+  <router-view/>
+</template>
+
+
+<style scoped>
+
+
+</style>
---
+++ vue-engineering-way/src/views/Friend.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+
+<template>
+    <div class="friend">
+        <p>我的朋友</p>
+        <p>我的朋友</p>
+        <p>我的朋友...</p>
+    </div>
+</template>
+
+
+<style scoped></style>
