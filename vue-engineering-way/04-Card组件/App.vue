d418f4b4acac4955114945778e4fd581ac9757de
xiaoli79 <3099592185@qq.com>
2026-06-01 23:35:28 +0800

    组件

---
+++ vue-engineering-way/04-Card组件/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-card style="max-width: 480px">
+
+
+    <!-- 头部:剧名插槽header进行分发 -->
+    <template #header>
+      <div class="card-header">
+        <span>Card name</span>
+      </div>
+    </template>
+    <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+
+    <!-- 尾部：具名插槽footer进行分发 -->
+    <template #footer>Footer content</template>
+  </el-card>
+</template>
