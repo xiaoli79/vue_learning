3277ae1ef91d0eac7b27607a6b44e55d57cadad1
xiaoli79 <3099592185@qq.com>
2026-06-02 19:31:48 +0800

    组件

--- vue-engineering-way/src/App.vue
+++ vue-engineering-way/src/App.vue
@@ -1,109 +1,39 @@
 <template>
-  <div class="flex items-center mb-4">
-    <el-radio-group v-model="size" class="mr-4">
-      <el-radio-button value="default">default</el-radio-button>
-      <el-radio-button value="large">large</el-radio-button>
+  <el-button plain @click="dialogVisible = true">
+    Click to open the Dialog
+  </el-button>
 
-      <el-radio-button value="small">small</el-radio-button>
-    </el-radio-group>
-    <div>
-      background:
-      <el-switch v-model="background" class="ml-2" />
-    </div>
-    <div class="ml-4">
-      disabled: <el-switch v-model="disabled" class="ml-2" />
-    </div>
-  </div>
-
-  <hr class="my-4" />
-
-  <div class="demo-pagination-block">
-    <div class="demonstration">Total item count</div>
-    <el-pagination
-      v-model:current-page="currentPage1"
-      :page-size="100"
-      :size="size"
-      :disabled="disabled"
-      :background="background"
-      layout="total, prev, pager, next"
-      :total="1000"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-    />
-  </div>
-  <div class="demo-pagination-block">
-    <div class="demonstration">Change page size</div>
-    <el-pagination
-      v-model:current-page="currentPage2"
-      v-model:page-size="pageSize2"
-      :page-sizes="[100, 200, 300, 400]"
-      :size="size"
-      :disabled="disabled"
-      :background="background"
-      layout="sizes, prev, pager, next"
-      :total="1000"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-    />
-  </div>
-  <div class="demo-pagination-block">
-    <div class="demonstration">Jump to</div>
-    <el-pagination
-      v-model:current-page="currentPage3"
-      v-model:page-size="pageSize3"
-      :size="size"
-      :disabled="disabled"
-      :background="background"
-      layout="prev, pager, next, jumper"
-      :total="1000"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-    />
-  </div>
-  <div class="demo-pagination-block">
-    <div class="demonstration">All combined</div>
-    <el-pagination
-      v-model:current-page="currentPage4"
-      v-model:page-size="pageSize4"
-      :page-sizes="[100, 200, 300, 400]"
-      :size="size"
-      :disabled="disabled"
-      :background="background"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="400"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-    />
-  </div>
+  <el-dialog
+    v-model="dialogVisible"
+    title="Tips"
+    width="500"
+    :before-close="handleClose"
+  >
+    <span>This is a message</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { ElMessageBox } from 'element-plus'
 
-const currentPage1 = ref(5)
-const currentPage2 = ref(5)
-const currentPage3 = ref(5)
-const currentPage4 = ref(4)
-const pageSize2 = ref(100)
-const pageSize3 = ref(100)
-const pageSize4 = ref(100)
-const size = ref('default')
-const background = ref(false)
-const disabled = ref(false)
+const dialogVisible = ref(false)
 
-const handleSizeChange = (val) => {
-  console.log(`${val} items per page`)
-}
-const handleCurrentChange = (val) => {
-  console.log(`current page: ${val}`)
+const handleClose = (done) => {
+  ElMessageBox.confirm('Are you sure to close this dialog?')
+    .then(() => {
+      done()
+    })
+    .catch(() => {
+      // catch error
+    })
 }
 </script>
-
-<style scoped>
-.demo-pagination-block + .demo-pagination-block {
-  margin-top: 10px;
-}
-.demo-pagination-block .demonstration {
-  margin-bottom: 16px;
-}
-</style>
